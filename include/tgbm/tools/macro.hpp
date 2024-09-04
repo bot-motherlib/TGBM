1adee2aa17c0151f260ccd6ae73c1532cf44b692
sabudilovskiy <sabudilovskiy@yandex.ru>
2024-09-04 23:24:50 +0300

    Debug macro (#8)
    Add debug macros

--- include/tgbm/tools/macro.hpp
+++ include/tgbm/tools/macro.hpp
@@ -7,3 +7,20 @@
 #else
 #define TGBM_TRIVIAL_ABI
 #endif
+
+#ifndef TGBM_ENABLE_DEBUG
+#define TGBM_ENABLE_DEBUG NDEBUG
+#endif
+
+#ifdef TGBM_ENABLE_DEBUG
+// clang-format off
+#define TGBM_ON_DEBUG(...) do { [&]() __VA_ARGS__(); } while (false)
+#define TGBM_DEBUG_FIELD(name, ...) __VA_ARGS__ name
+
+#else
+
+#define TGBM_ON_DEBUG(...) do {} while (false)
+#define TGBM_DEBUG_FIELD(name, ...)
+// clang-format on
+
+#endif
