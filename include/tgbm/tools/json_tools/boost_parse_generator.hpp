6662b0a52ab642e0a1992ea22780b8a6ef8859a3
sabudilovskiy <sabudilovskiy@yandex.ru>
2024-10-08 20:59:00 +0000

    -

--- include/tgbm/tools/json_tools/boost_parse_generator.hpp
+++ include/tgbm/tools/json_tools/boost_parse_generator.hpp
@@ -14,20 +14,6 @@ struct formatter<::boost::json::string_view> : formatter<std::string_view> {
 namespace tgbm::json::boost {
 namespace details {
 
-// template <typename... Args>
-// void log_event_helper(std::string_view func, Args&&... args) {
-//   std::string fmt_str = "{}";
-//   for (std::size_t i = 0; i < sizeof...(args); i++) {
-//     fmt_str.append(" [{}]");
-//   }
-//   fmt_str.append("\n");
-//   ::fmt ::vformat_to(::std::ostreambuf_iterator(std ::cout), fmt_str, fmt::make_format_args(func,
-//   args...));
-// }
-
-// #define $log_event(...) log_event_helper(__PRETTY_FUNCTION__ __VA_OPT__(, ) __VA_ARGS__)
-#define $log_event(...)
-
 inline dd::generator<generator_parser::nothing_t> unite_generate(
     std::vector<char>& buf, bool& part, generator_parser::event_holder& holder,
     dd::generator<generator_parser::nothing_t>& field_gen,
@@ -80,12 +66,10 @@ struct wait_handler {
   wait_handler(wait_handler&&) = delete;
 
   bool on_document_begin(error_code& ec) {
-    $log_event();
     return true;
   }
 
   bool on_document_end(error_code& ec) {
-    $log_event();
     return true;
   }
 
@@ -95,35 +79,30 @@ struct wait_handler {
   }
 
   bool on_array_begin(error_code& ec) {
-    $log_event();
     event.got = wait_e::array_begin;
     resume_generator();
     return true;
   }
 
   bool on_array_end(std::size_t n, error_code& ec) {
-    $log_event();
     event.got = wait_e::array_end;
     resume_generator();
     return true;
   }
 
   bool on_object_begin(error_code& ec) {
-    $log_event();
     event.got = wait_e::object_begin;
     resume_generator();
     return true;
   }
 
   bool on_object_end(std::size_t n, error_code& ec) {
-    $log_event();
     event.got = wait_e::object_end;
     resume_generator();
     return true;
   }
 
   bool on_string(string_view s, std::size_t n, error_code& ec) {
-    $log_event(s);
     event.got = wait_e::string;
     event.str_m = s;
     resume_generator();
@@ -131,8 +110,6 @@ struct wait_handler {
   }
 
   bool on_key(string_view s, std::size_t n, error_code& ec) {
-    $log_event(s);
-
     event.got = wait_e::key;
     event.key_m = s;
     resume_generator();
@@ -140,7 +117,6 @@ struct wait_handler {
   }
 
   bool on_int64(int64_t i, string_view s, error_code& ec) {
-    $log_event(i);
     event.got = wait_e::int64;
     event.int_m = i;
     resume_generator();
@@ -148,7 +124,6 @@ struct wait_handler {
   }
 
   bool on_uint64(uint64_t u, string_view s, error_code& ec) {
-    $log_event(u);
     event.got = wait_e::uint64;
     event.uint_m = u;
     resume_generator();
@@ -156,7 +131,6 @@ struct wait_handler {
   }
 
   bool on_double(double d, string_view s, error_code& ec) {
-    $log_event(d);
     event.got = wait_e::double_;
     event.double_m = d;
     resume_generator();
@@ -164,7 +138,6 @@ struct wait_handler {
   }
 
   bool on_bool(bool b, error_code& ec) {
-    $log_event(b);
     event.got = wait_e::bool_;
     event.bool_m = b;
     resume_generator();
@@ -172,7 +145,6 @@ struct wait_handler {
   }
 
   bool on_null(error_code& ec) {
-    $log_event();
     event.got = wait_e::null;
     resume_generator();
     return true;
@@ -184,13 +156,11 @@ struct wait_handler {
     if (!part) {
       gen = unite_generate(buf, part, event, gen, std::move(gen));
     }
-    $log_event(s);
     resume_generator();
     return true;
   }
 
   bool on_comment_part(string_view s, error_code& ec) {
-    $log_event(s);
     return true;
   }
 
@@ -200,18 +170,15 @@ struct wait_handler {
     if (!part) {
       gen = unite_generate(buf, part, event, gen, std::move(gen));
     }
-    $log_event(s);
     resume_generator();
     return true;
   }
 
   bool on_number_part(string_view s, error_code& ec) {
-    $log_event(s);
     return true;
   }
 
   bool on_comment(string_view s, error_code& ec) {
-    $log_event(s);
     return true;
   }
 };
