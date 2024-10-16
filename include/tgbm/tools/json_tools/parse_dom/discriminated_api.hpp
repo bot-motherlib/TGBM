70e4205f938a9b6a39b7b7e99a03143c78b53fc3
sabudilovskiy <sabudilovskiy@yandex.ru>
2024-10-16 20:15:55 +0000

    fix format and safe_cast

--- include/tgbm/tools/json_tools/parse_dom/discriminated_api.hpp
+++ include/tgbm/tools/json_tools/parse_dom/discriminated_api.hpp
@@ -25,11 +25,6 @@ struct parser<T> {
     if (!Traits::is_string(d_ref)) [[unlikely]] {
       Traits::on_error();
     }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> e14721b (first impl)
     auto str = Traits::get_string(d_ref);
 
     T::discriminate(str, [&]<typename U>() {
