bbb0f72c74abc8ced4d09d0122ce498a83666202
Kelbon Nik <kelbonage@gmail.com>
2024-12-23 20:21:08 +0400

    ?

--- include/tgbm/jsons/parse_sax/common_api.hpp
+++ include/tgbm/jsons/parse_sax/common_api.hpp
@@ -13,7 +13,8 @@ namespace tgbm::json {
 namespace noexport {
 
 template <typename T, size_t N, size_t... Is>
-inline std::bitset<::pfr_extension::tuple_size_v<T>> make_required_mask(std::index_sequence<Is...>) {
+[[gnu::noinline]] inline std::bitset<::pfr_extension::tuple_size_v<T>> make_required_mask(
+    std::index_sequence<Is...>) {
   std::bitset<N> res;
   (res.set(Is, T::is_mandatory_field(pfr_extension::element_name_v<Is, T>)), ...);
   return res;
