e6faacf0bc467ac0d979c61bc73cded9529fd980
kelbon <you@example.com>
2025-09-14 18:47:22 +0500

    use levels, not strings in logger

--- include/tgbm/logger.hpp
+++ include/tgbm/logger.hpp
@@ -1,15 +1,10 @@
 #pragma once
-/*
-  simplest replacable logger
-  To replace logger user should create tgbm_replace/logger.hpp file
-  and add ../tgbm_replace into include dirs
-*/
 
 #include <tgbm/utils/macro.hpp>
 #if __has_include(<tgbm_replace/logger.hpp>)
   #include <tgbm_replace/logger.hpp>
   #ifndef TGBM_LOG
-    #error <tgbm_replace/logger.hpp> must define TGBM_LOG(FMT_STR, ...ARGS)
+    #error <tgbm_replace/logger.hpp> must define TGBM_LOG(LOGTYPE, FMT_STR, ...ARGS)
   #endif
 #else
 
@@ -28,33 +23,35 @@ TGBM_GCC_WORKAROUND void do_log(fmt::format_string<Args...> fmtstr, Args&&... ar
     ::fmt::println(stdout, "FMT_ERR");
   }
 }
+
 }  // namespace tgbm::noexport
 
 // try catch is workaround fmt bug with "invalid" utf (exception on valid boost asio error strings)
-  #define TGBM_LOG(FMT_STR, ...) ::tgbm::noexport::do_log(FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+  #define TGBM_LOG(TYPE, FMT_STR, ...) \
+    ::tgbm::noexport::do_log("[" #TYPE "] " FMT_STR __VA_OPT__(, ) __VA_ARGS__)
 
 #endif
 
 #ifndef TGBM_LOG_INFO
-  #define TGBM_LOG_INFO(FMT_STR, ...) TGBM_LOG("[INFO] " FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+  #define TGBM_LOG_INFO(FMT_STR, ...) TGBM_LOG(INFO, FMT_STR __VA_OPT__(, ) __VA_ARGS__)
 #endif
 
 #ifndef TGBM_LOG_ERROR
-  #define TGBM_LOG_ERROR(FMT_STR, ...) TGBM_LOG("[ERROR] " FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+  #define TGBM_LOG_ERROR(FMT_STR, ...) TGBM_LOG(ERROR, FMT_STR __VA_OPT__(, ) __VA_ARGS__)
 #endif
 
 #ifndef TGBM_LOG_WARN
-  #define TGBM_LOG_WARN(FMT_STR, ...) TGBM_LOG("[WARN] " FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+  #define TGBM_LOG_WARN(FMT_STR, ...) TGBM_LOG(WARN, FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+#endif
+
+#ifndef TGBM_LOG_TRACE
+  #define TGBM_LOG_TRACE(FMT_STR, ...) TGBM_LOG(TRACE, FMT_STR __VA_OPT__(, ) __VA_ARGS__)
 #endif
 
 #ifndef TGBM_LOG_DEBUG
   #ifndef NDEBUG
-    #define TGBM_LOG_DEBUG(FMT_STR, ...) TGBM_LOG("[DEBUG] " FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+    #define TGBM_LOG_DEBUG(FMT_STR, ...) TGBM_LOG(DEBUG, FMT_STR __VA_OPT__(, ) __VA_ARGS__)
   #else
     #define TGBM_LOG_DEBUG(FMT_STR, ...) (void)0
   #endif
-#endif
-
-#ifndef TGBM_HTTP2_LOG
-  #define TGBM_HTTP2_LOG(TYPE, STR, ...) TGBM_LOG_##TYPE("[HTTP2] " STR __VA_OPT__(, ) __VA_ARGS__)
 #endif
